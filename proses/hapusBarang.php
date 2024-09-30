5101868879cd7125bce574c8955592039c19d703
Heliandra9 <148680937+Heliandra9@users.noreply.github.com>
2024-09-30 13:37:58 +0700

    Delete proses directory

--- proses/hapusBarang.php
+++
@@ -1,6 +0,0 @@
-<?php
-require('../config/koneksi.php');
-
-$id= $_POST['id'];
-
-$query = $con->query("DELETE FROM tbl_obat WHERE id_obat = $id");
--- proses/hapusUser.php
+++
@@ -1,6 +0,0 @@
-<?php
-require('../config/koneksi.php');
-
-$id = $_POST['id'];
-
-$qHapus = $con->query("DELETE FROM `tbl_user` WHERE id_user = $id");
